9fa61854c62c1827a1e298551c800c59e8134b97
Matthew Rizzini <matthewrizzini@gmail.com>
2018-05-16 19:13:59 -0400

    starting section 6

---
+++ Section6/Interfaces/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Interfaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // INTERFACES
+
+            // A language construct that is similar to a class (in terms of syntax) but is fundamentally different
+
+            // public interface ITaxCalculator
+
+            // all interfaces start with I, it is a convention
+
+            // do not have implentation
+            // do not have access modifiers
+
+            // This is to buuld loosely-coupled apps
+            // The components are not tightly related to each other so the change in one of the components is easier and has less or 0 impact on other components
+
+            // Any chef can work in a restaurant. If the chef quits then he can be replaced and any chef can come in and replace
+            // If the restaurant only could have Chef John, it would have a big impact if Chef John left
+
+            // Interfaces reduce coupling
+        }
+    }
+}
